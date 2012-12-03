4d680b2f0a50d199b9ec6832bdd24c3eab6de905
Kewin Pedersen <Kewinp@live.dk>
2012-12-03 22:21:50 +0100

    First commit

---
+++ EmptyProject/KekClassTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmptyProject
+{
+    class KekClassTest
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Kasra do a dance");
+        }
+    }
+}
